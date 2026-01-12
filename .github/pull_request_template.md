660afcfcebdef6bf91f483afe908558c42126144
kartiksharma-rupyy <kartik.sharma@rupyy.com>
2026-01-12 16:28:45 +0530

    Initial commit with Fibonacci TS code and PR template

---
+++ .github/pull_request_template.md
@@ -0,0 +1,16 @@
+# Pull Request
+
+## Problem Statement
+<!-- to implement the fibo series in the following ts file -->
+
+## Solution Overview
+<!-- used the recursion approch for the fibo series and added both the edge cases in browser only and manual way -->
+
+## Testing Done
+<!-- IN the first case i checked the edge cases of n==0 and n==1 and also it should be a positive number -->
+
+## Checklist
+- [ ] Code is linted
+- [ ] Code builds successfully
+- [ ] Functionality verified for edge cases (e.g., n = 0, n = 1)
+- [ ] PR description filled out
---
+++ dist/fibo.js
@@ -0,0 +1,24 @@
+"use strict";
+function fibo(n) {
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 0)
+        return 0;
+    // basic two base case for n==0,n==1;
+    return fibo(n - 1) + fibo(n - 2);
+}
+// browser friendly code
+// const number = prompt("Enter the nth element ");
+// // condition for the input to be a number 
+// if (number !== null) {
+//   const num: number = Number(number);
+//   if (isNaN(num) || num < 0 || !Number.isInteger(num)) {
+//     console.log("non-negative number not allowed");
+//   }
+//   else{
+//     console.log(fibo(num));
+//   }
+// }
+console.log(fibo(4));
+console.log(fibo(5));
